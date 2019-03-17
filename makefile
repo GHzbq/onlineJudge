2c49c63071d9b3f06114ba7c938e0066eb7121d4
GHzbq <eric_qiushui@126.com>
2019-03-17 13:31:21 +0800

    upload test.cpp

--- makefile
+++ makefile
@@ -1,7 +1,12 @@
-hello: helloWorld.cpp
-	g++ $^ -o $@ -std=c++11 -lpthread
+all: test
+
+
+main: main.cpp httplib.h compiler.hpp
+	g++ main.cpp -o $@ -g -std=c++11 -lpthread -ljsoncpp
+test: test.cpp httplib.h
+	g++ test.cpp -o $@ -std=c++11 -lpthread -ljsoncpp
 
 .PHONY: clean
 
-clean: hello
+clean: test
 	rm -f $^
---
+++ test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <jsoncpp/json/json.h>
+#include "httplib.h"
+#include "compiler.hpp"
+
+void test1()
+{
+    using namespace httplib;
+    Server server;
+    server.Post("/compile", [](const Request& req, Response& resp)
+               {
+               // (void) req;
+               // resp.set_content("<html>hello<html>", "text/html");
+                    // (void)req;
+                    std::unordered_map<std::string, std::string> bodyKV;
+                    util::urlUtil::parseBody(req.body, &bodyKV);
+                    Json::Value req_json;
+                    for(const auto & e: bodyKV)
+                    {
+                        req_json[e.first] = e.second;
+                    }
+                    Json::Value resp_json;
+                    compiler::compilerAndRun(req_json, &resp_json);
+                    Json::FastWriter wrt;
+                    resp.set_content(wrt.write(resp_json), "text/plain");
+               });
+    server.set_base_dir("./wwroot");
+    server.listen("0.0.0.0", 9092);
+}
+
+void test2()
+{
+    std::string code;
+    util::fileUtil::read("./text", &code);
+    std::cout << "text:[ " << code << " ]" << std::endl;
+    std::string str = "#include <stdio.h> \n int main(void){printf(\"hehe\"); return 0}";
+    util::fileUtil::write("./text", str);
+    util::fileUtil::read("./text", &code);
+    std::cout << "text:[ " << code << " ]" << std::endl;
+}
+
+int main(void)
+{
+    test1();
+    return 0;
+}
